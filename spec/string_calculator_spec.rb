337cb6ee809fcd7ab8b8de3d93a7a3e2fc589ae7
Ashish Ghate <ashishghate90@gmail.com>
2025-05-21 21:26:21 +0530

    added test-case for single digit input
    update add method for single digit input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe StringCalculator do
     it "returns 0 for empty string" do
       expect(calculator.add("")).to eq(0)
     end
+
+    it "returns the number itself for single number input" do
+      expect(calculator.add("4")).to eq(4)
+    end
   end
 end
