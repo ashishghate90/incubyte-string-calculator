b764359f214f213c4ec9d2cebbefb3682ae262f6
Ashish Ghate <ashishghate90@gmail.com>
2025-05-21 22:34:22 +0530

    added test-cases to raises error for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -28,5 +28,9 @@ RSpec.describe StringCalculator do
     it "supports custom delimiter" do
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
+
+    it "raises error for negative numbers" do
+      expect { calculator.add("1,-2,3,-5") }.to raise_error("negative numbers not allowed -2,-5")
+    end
   end
 end
