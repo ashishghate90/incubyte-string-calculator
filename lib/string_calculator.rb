b9f9480d850e58005c561255810596cdd3ce2a42
Ashish Ghate <ashishghate90@gmail.com>
2025-05-21 21:45:01 +0530

    updated add method for sum of comma-separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    return numbers.to_i
+    return numbers.to_i unless numbers.include?(",")
+
+    numbers.split(",").map(&:to_i).sum
   end
 end
